3e8839f1418aa6900ba62cb159df1fc7e3505b55
Rachit Sen <rachitsen15@gmail.com>
2024-08-31 01:51:28 +0530

    initial commit

---
+++ App.jsx
@@ -0,0 +1,25 @@
+
+
+import React from 'react';
+import {
+  SafeAreaView,
+  ScrollView,
+  StatusBar,
+  StyleSheet,
+  Text,
+  useColorScheme,
+  View,
+} from 'react-native';
+
+const App = () => {
+  return(
+    <View>
+ <Text style={{fontSize:30}}>Hello  React Native</Text>
+<Text style={{fontSize:30}}>Hello  React Native</Text>
+
+    </View>
+   
+  );
+};
+export default App;
+
